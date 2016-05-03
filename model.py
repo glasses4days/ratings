d3d568e725cca484ea792adf55903a4cde5f2e53
Hackbright Student <info@hackbrightacademy.com>
2016-05-03 11:39:59 -0700

    Added User-Rating relationship

--- model.py
+++ model.py
@@ -40,15 +40,34 @@ class Movie(db.Model):
     released_at = db.Column(db.DateTime, nullable=True)
     imdb_url = db.Column(db.String(500), nullable=True)
 
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<Movie movie_id=%s title=%s released_at=%s imdb_url=%s>" % (self.movie_id,
+            self.title, self.released_at, self.imdb_url)
+
 class Rating(db.Model):
     """Movie ratings, many per user"""
 
     __tablename__ = "ratings"
 
     rating_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    movie_id = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, nullable=False)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.movie_id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
     score = db.Column(db.Integer, nullable=False)
+
+    #Define relationship to user
+    user = db.relationship("User", backref=db.backref('ratings', order_by=rating_id))
+
+    #Define relationship to movie
+    movie = db.relationship("Movie", backref=db.backref('ratings', order_by=rating_id))
+
+    def __repr__(self):
+        """Provide helpful representation when printed"""
+
+        return "<Rating rating_id=%s movie_id=%s user_id=%s score=%s>" % (self.rating_id,
+            self.movie_id, self.user_id, self.score)
+
 
 ##############################################################################
 # Helper functions
